2fb2734942cbd5a807a2bfdbc07ae8a117ed6ed3
Santiago Pereira <spereira.c@aes.com>
2024-10-25 01:12:46 -0500

    feat: Update lint scripts to remove extension specification and add file pattern.

---
+++ eslint.config.mjs
@@ -0,0 +1,40 @@
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [{
+    ignores: ["**/node_modules/", "**/dist/", "**/coverage/", "**/generated/"],
+}, ...compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended"), {
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.node,
+        },
+
+        parser: tsParser,
+        ecmaVersion: 2020,
+        sourceType: "module",
+    },
+
+    rules: {
+        "no-console": "warn",
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/no-explicit-any": "warn",
+        "@typescript-eslint/no-unused-vars": "error",
+    },
+}];
